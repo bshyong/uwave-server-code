ed1786b262e4588c0a34246f01bf9131376d78b8
Ben Shyong <likeachild@gmail.com>
2011-09-17 01:16:35 -0400

    project directory mixup

--- db/migrate/20110917045753_add_submission_id_to_total_submission_votes.rb
+++
@@ -1,9 +0,0 @@
-class AddSubmissionIdToTotalSubmissionVotes < ActiveRecord::Migration
-  def self.up
-    add_column :total_submission_votes, :submission_id, :integer
-  end
-
-  def self.down
-    remove_column :total_submission_votes, :submission_id
-  end
-end
